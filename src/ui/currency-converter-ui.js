74e8f4c6c1563e837a6eca2bde4c219eac500e40
Hung Nguyen <hung.nguyen@sentifi.com>
2017-05-19 16:09:15 +0700

    render data

--- src/ui/currency-converter-ui.js
+++ src/ui/currency-converter-ui.js
@@ -4,12 +4,25 @@ class CurrencyConvertUI extends React.Component {
 
   render() {
     console.log('PROPS: ', this.props);
+    const obj = this.props.quotes;
+    let columns;
+    if (obj) {
+      columns = Object.keys( obj ).map( (p,i) => {
+        return (<div key={p}>{p}  {obj[p]}</div>);
+      });
+    }
     return (
-      <div>Items:
-        <ul>
-          
-        </ul>
-      </div>
+       <div className="row">
+            <div className="col-md-4">
+                <p><img className="img-responsive" src="./images/logo.png" /></p>
+            </div>
+            <div className="col-md-8">
+                <div className="page-header">
+                    <h1>Currency <small>Converter</small></h1>
+                    {columns}
+                </div>
+            </div>
+        </div>
     );
   }
 }
