a0707668adb5f82bc2083dbbfb26103dfd626a76
Hung Nguyen <hung.nguyen@sentifi.com>
2017-05-22 22:39:27 +0700

    refactor fetch api

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -22,35 +22,35 @@ class CurrencyConvert extends React.Component {
     const params = {
       access_key: '9af2d5555fd753d5b3fccb3a4c7a3341'
     };
-    let obj1 = {}
+
     _getData(params).then(data => {
       this.setState(
         Object.assign({}, this.state, { res: data })
         );
+      _getCurrencyList(params).then(currencyList => {
+        const newState = this.state;
+        const currencies = currencyList.currencies;
+        newState.res.currencies = currencies;
+        this.setState(newState);
+      });
     });
-
-    _getCurrencyList(params).then(currencyList => {
-      console.log('List', currencyList);
-      Object.assign({}, ...this.state, { list: currencyList })
-    });
-    console.log('DATA', obj1);
   }
 
   processData() {
-    const data = {
-      
-    };
+    const data = {};
 
     if (this.state.res) {
       const res = this.state.res;  
       data.quotes = res.quotes;
       data.source = res.source;
+      data.currencies = res.currencies;
     }
     return data;
   }
 
   render() {
     const data = this.processData();
+   
     return (
       <CurrencyUI {...data} 
         onChange={this.onChange}
