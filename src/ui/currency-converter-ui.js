cc3efa4f63a1ed0a276e7e3ff244797701cf5f30
Hung Nguyen <hung.nguyen@sentifi.com>
2017-05-23 15:07:40 +0700

    feat create form component

--- src/ui/currency-converter-ui.js
+++ src/ui/currency-converter-ui.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CurrencyTable from './currency-table-ui';
+import CurrencyForm from './currency-form-ui';
 
 class CurrencyConvertUI extends Component {
 
@@ -17,57 +18,24 @@ class CurrencyConvertUI extends Component {
   }
 
   render() {
-    const currencyList = this.props.currencies;
-    const rate = this.props.rate || 0;
     const time = this.props.timestamp;
-    let currency;
-
-    if (currencyList) {
-      currency = Object.keys(currencyList).map((curr, i) => {
-        return (
-          <option value={curr} key={i} title={currencyList[curr]}>{curr}</option>
-        );
-      });
-    }
 
     return (
        <div className="row">
-            <div className="col-md-4 col-sm-4 col-xs-12">
-                <p><img className="img-responsive" src={require('../images/logo.png')} /></p>
+          <div className="col-md-4 col-sm-4 col-xs-12">
+            <p><img className="img-responsive" src={require('../images/logo.png')} /></p>
+          </div>
+          <div className="col-md-8 col-sm-8 col-xs-12">
+            <div className="page-header">
+              <h1>Currency <small>Converter</small></h1>
+              Updated: { this.renderTime(time) }
             </div>
-            <div className="col-md-8 col-sm-8 col-xs-12">
-              <div className="page-header">
-                <h1>Currency <small>Converter</small></h1>
-                Updated: { this.renderTime(time) }
-              </div>
-                <form className="form-horizontal">
-                  <div className="form-group">
-                  <div className="col-md-6 col-sm-6 col-xs-6">
-                    <input type="text" className="form-control" value="1" readOnly/>
-                  </div>
-                  <div className="col-md-6 col-sm-6 col-xs-6">
-                    <select className="form-control" disabled>
-                      <option value='USD' title='US Dollar'>USD</option>
-                    </select>
-                  </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="col-md-6 col-sm-6 col-xs-6">
-                      <input type="text" className="form-control" value={rate} />
-                    </div>
-                    <div className="col-md-6 col-sm-6 col-xs-6">
-                      <select className="form-control" onChange={this.props.onChange.bind(this)} >
-                        <option value="">Select currency</option>
-                        { currency }
-                      </select>
-                    </div>
-                  </div>
-                </form>
-            </div>
-            <div className="col-md-12 col-sm-12 col-xs-12">
-              <CurrencyTable {...this.props} />       
-            </div>
-        </div>
+              <CurrencyForm {...this.props} />
+          </div>
+          <div className="col-md-12 col-sm-12 col-xs-12">
+            <CurrencyTable {...this.props} />       
+          </div>
+      </div>
     );
   }
 }
---
+++ src/ui/currency-form-ui.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class CurrencyForm extends Component {
+  render() {
+    const currencyList = this.props.currencies;
+    const rate = this.props.rate || 0;
+    let currency;
+
+    if (currencyList) {
+      currency = Object.keys(currencyList).map((curr, i) => {
+        return (
+          <option value={curr} key={i} title={currencyList[curr]}>{curr}</option>
+        );
+      });
+    }
+    
+    return (
+      <form className="form-horizontal">
+        <div className="form-group">
+        <div className="col-md-6 col-sm-6 col-xs-6">
+          <input type="text" className="form-control" value="1" readOnly/>
+        </div>
+        <div className="col-md-6 col-sm-6 col-xs-6">
+          <select className="form-control" disabled>
+            <option value='USD' title='US Dollar'>USD</option>
+          </select>
+        </div>
+        </div>
+        <div className="form-group">
+          <div className="col-md-6 col-sm-6 col-xs-6">
+            <input type="text" className="form-control" value={rate} />
+          </div>
+          <div className="col-md-6 col-sm-6 col-xs-6">
+            <select className="form-control" onChange={this.props.onChange.bind(this)} >
+              <option value="">Select currency</option>
+              { currency }
+            </select>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CurrencyForm;
